1f4a400926a61dde261e5eb9db130dcf3cfcca0c
Maxim Ten <maximten94@gmail.com>
2022-08-07 23:00:26 +0600

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use {
+        'kyazdani42/nvim-tree.lua'
+    }
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+end)
